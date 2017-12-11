ae6c9fcb39471fdaccb4018f180fc2768f2e304e
qibutian <603986018@qq.com>
2017-12-11 23:01:26 +0800

    tj

--- qiahao-lib/src/net/duohuo/dhroid/net/model/JSONObjectN.java
+++
@@ -1,9 +0,0 @@
-package net.duohuo.dhroid.net.model;
-
-import java.io.Serializable;
-
-public class JSONObjectN extends org.json.JSONObject implements Serializable {
-
-	private static final long serialVersionUID = 5213230387175987834L;
-
-}
--- qiahao-lib/src/net/duohuo/dhroid/view/ViewHelper.java
+++
@@ -1,5 +0,0 @@
-package net.duohuo.dhroid.view;
-
-public class ViewHelper {
-
-}
